eb78093e20f8d53061641c53e569cc66306ec7dc
kivan <kirill.ivanin00@yandex.ru>
2022-02-15 17:13:11 +0300

    Setup user permissions and there seeders (Admin password flashed)

--- database/seeders/UserArticleCommentSeeder.php
+++ database/seeders/UserArticleCommentSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Article;
 use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UserArticleCommentSeeder extends Seeder
 {
@@ -15,10 +17,24 @@ class UserArticleCommentSeeder extends Seeder
      */
     public function run()
     {
+        Role::create(['name'=>'Admin']);
+        Role::create(['name'=>'User']);
+
         Article::factory(10)
             ->has(Comment::factory(3)
                 ->has(Comment::factory(2)->hasReplies(1), 'replies')
                 )
             ->create();
+
+        /**
+         * @var $user User
+         */
+        $user = User::factory()->create([
+            'name' => 'kivan',
+            'email'=>'kirill.ivanin00@yanartdex.ru',
+            'password' => 'password'
+        ]);
+
+        $user->assignRole('Admin');
     }
 }
